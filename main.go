9413cff91f073a959920a1fcb349d563fe0185f3
flvsantos15 <flvsantos300@gmail.com>
2025-01-03 14:54:45 -0300

    chore: read csv file

--- main.go
+++ main.go
@@ -2,8 +2,10 @@ package main
 
 import (
 	"bufio"
+	"encoding/csv"
 	"fmt"
 	"os"
+	"strconv"
 )
 
 type Question struct {
@@ -24,7 +26,6 @@ func (g *GameState) Int() {
 	reader := bufio.NewReader(os.Stdin)
 
 	name, err := reader.ReadString('\n')
-
 	if err != nil {
 		panic("Error ao ler a string")
 	}
@@ -34,7 +35,46 @@ func (g *GameState) Int() {
 	fmt.Printf("Vamos ao jogo %s", g.Name)
 }
 
+func (g *GameState) ProccessCSV() {
+	f, err := os.Open("quiz-go.csv")
+	if err != nil {
+		panic("Error ao abrir o arquivo")
+	}
+
+	defer f.Close()
+
+	reader := csv.NewReader(f)
+	records, err := reader.ReadAll()
+	if err != nil {
+		panic("Error ao ler o arquivo")
+	}
+
+	for index, record := range records {
+		fmt.Println(index, record)
+		if index > 0 {
+			question := Question {
+				Text: record[0],
+				Options: record[1:5],
+				Answer: toInt(record[5]),
+			}
+
+			g.Questions = append(g.Questions, question)
+		}
+	}
+}
+
 func main() {
 	game := &GameState{}
+	game.ProccessCSV()
 	game.Int()
+	fmt.Println(game.Questions)
+}
+
+func toInt(s string) int {
+	i, err := strconv.Atoi(s)
+	if err != nil {
+		panic(err)
+	}
+
+	return i
 }
