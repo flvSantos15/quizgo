805cfdc3122f02b1535336969f5734ac42458cf9
flvsantos15 <flvsantos300@gmail.com>
2025-01-03 14:43:18 -0300

    chore: read string from terminal

--- main.go
+++ main.go
@@ -1,7 +1,40 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+type Question struct {
+	Text string
+	Options []string
+	Answer int
+}
+
+type GameState struct {
+	Name string
+	Points string
+	Questions []Question
+}
+
+func (g *GameState) Int() {
+	fmt.Println("Seja bem vindo(a) ao quiz")
+	fmt.Println("Escreva o seu nome: ")
+	reader := bufio.NewReader(os.Stdin)
+
+	name, err := reader.ReadString('\n')
+
+	if err != nil {
+		panic("Error ao ler a string")
+	}
+
+	g.Name = name
+
+	fmt.Printf("Vamos ao jogo %s", g.Name)
+}
 
 func main() {
-	fmt.Println("Hello, World!")
+	game := &GameState{}
+	game.Int()
 }
